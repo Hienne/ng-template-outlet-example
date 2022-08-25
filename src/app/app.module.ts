8f3a3728ae031ff4aaa2d083fa840d3d6b810dff
hien dep trai <nguyenvanhiencntt3@gmail.com>
2022-08-25 11:24:02 +0700

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './shared/ui/table/table.component';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/home/home.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  employees = [
+    { firstName: 'Employee', lastName: 'One' },
+    { firstName: 'Employee', lastName: 'Two' },
+    { firstName: 'Employee', lastName: 'Three' },
+    { firstName: 'Employee', lastName: 'Four' },
+    { firstName: 'Employee', lastName: 'Five' },
+    { firstName: 'Employee', lastName: 'Six' },
+  ];
+
+  inventory = [
+    {
+      plu: 110,
+      supplier: 'X Corp',
+      name: 'Table extender',
+      inStock: 500,
+      price: 50,
+      currency: 'GBP',
+    },
+    {
+      plu: 120,
+      supplier: 'X Corp',
+      name: 'Heated toilet seat',
+      inStock: 0,
+      price: 80,
+      currency: 'GBP',
+    },
+    {
+      plu: 155,
+      supplier: 'Y Corp',
+      name: 'Really good pencil',
+      inStock: 1,
+      price: 8000,
+      currency: 'AUD',
+    },
+  ];
+
+  purchaseItem(plu: number) {
+    console.log('handle purchase for', plu);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
